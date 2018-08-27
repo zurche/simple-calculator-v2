01fc6a83e0bbb59f14a11560f6022ea47db6c15f
Alejandro Zurcher <alejandro.zurcher@gmail.com>
2018-08-27 18:16:46 -0300

    Adding gitignore

--- app/build/generated/source/r/debug/android/support/constraint/R.java
+++ app/build/generated/source/r/debug/android/support/constraint/R.java
@@ -67,26 +67,26 @@ public final class R {
         public static final int layout_optimizationLevel = 0x7f0300ca;
     }
     public static final class id {
-        public static final int barrier = 0x7f09001e;
-        public static final int bottom = 0x7f090021;
-        public static final int chains = 0x7f090026;
-        public static final int dimensions = 0x7f090031;
-        public static final int direct = 0x7f090032;
-        public static final int end = 0x7f090035;
-        public static final int gone = 0x7f09003f;
-        public static final int invisible = 0x7f090047;
-        public static final int left = 0x7f090049;
-        public static final int none = 0x7f090052;
-        public static final int packed = 0x7f09005a;
-        public static final int parent = 0x7f09005b;
-        public static final int percent = 0x7f09005d;
-        public static final int right = 0x7f090062;
-        public static final int spread = 0x7f09007c;
-        public static final int spread_inside = 0x7f09007d;
-        public static final int standard = 0x7f090081;
-        public static final int start = 0x7f090082;
-        public static final int top = 0x7f090091;
-        public static final int wrap = 0x7f090097;
+        public static final int barrier = 0x7f09001f;
+        public static final int bottom = 0x7f090022;
+        public static final int chains = 0x7f090027;
+        public static final int dimensions = 0x7f090033;
+        public static final int direct = 0x7f090034;
+        public static final int end = 0x7f090039;
+        public static final int gone = 0x7f090046;
+        public static final int invisible = 0x7f09004e;
+        public static final int left = 0x7f090050;
+        public static final int none = 0x7f09005b;
+        public static final int packed = 0x7f090064;
+        public static final int parent = 0x7f090065;
+        public static final int percent = 0x7f090067;
+        public static final int right = 0x7f09006f;
+        public static final int spread = 0x7f09008b;
+        public static final int spread_inside = 0x7f09008c;
+        public static final int standard = 0x7f090090;
+        public static final int start = 0x7f090091;
+        public static final int top = 0x7f0900a1;
+        public static final int wrap = 0x7f0900a8;
     }
     public static final class styleable {
         public static final int[] ConstraintLayout_Layout = { 0x010100c4, 0x0101011f, 0x01010120, 0x0101013f, 0x01010140, 0x7f030037, 0x7f030038, 0x7f030046, 0x7f030059, 0x7f03005a, 0x7f030096, 0x7f030097, 0x7f030098, 0x7f030099, 0x7f03009a, 0x7f03009b, 0x7f03009c, 0x7f03009d, 0x7f03009e, 0x7f03009f, 0x7f0300a0, 0x7f0300a1, 0x7f0300a2, 0x7f0300a3, 0x7f0300a4, 0x7f0300a5, 0x7f0300a6, 0x7f0300a7, 0x7f0300a8, 0x7f0300a9, 0x7f0300aa, 0x7f0300ab, 0x7f0300ac, 0x7f0300ad, 0x7f0300ae, 0x7f0300af, 0x7f0300b0, 0x7f0300b1, 0x7f0300b2, 0x7f0300b3, 0x7f0300b4, 0x7f0300b5, 0x7f0300b6, 0x7f0300b7, 0x7f0300b8, 0x7f0300b9, 0x7f0300ba, 0x7f0300bb, 0x7f0300bc, 0x7f0300bd, 0x7f0300be, 0x7f0300c0, 0x7f0300c1, 0x7f0300c2, 0x7f0300c3, 0x7f0300c4, 0x7f0300c5, 0x7f0300c6, 0x7f0300c7, 0x7f0300ca };
