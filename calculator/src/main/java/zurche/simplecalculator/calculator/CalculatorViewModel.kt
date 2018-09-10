ceb7b537ecdb090e29b65c7167d7c0377f6460e0
Alejandro Zurcher <alejandro.zurcher@gmail.com>
2018-09-10 12:32:13 -0300

    Migrating architecture to ViewModel and LiveData

---
+++ calculator/src/main/java/zurche/simplecalculator/calculator/CalculatorViewModel.kt
@@ -0,0 +1,160 @@
+package zurche.simplecalculator.calculator
+
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.ViewModel
+import com.udojava.evalex.Expression
+import java.math.BigDecimal
+import java.util.*
+
+class CalculatorViewModel : ViewModel() {
+
+    private var mCurrentExpression: MutableLiveData<String>? = null
+    private var mResult: MutableLiveData<String>? = null
+
+    private var isNumberPositive = true
+
+    private val STRING_COMMA = "."
+    private val PERCENTAGE = "%"
+    private val SCIENTIFIC_NOTATION_CHAR = "E"
+    private val INFINITY = "Infinity"
+    private val validOperators = Arrays.asList('+', '-', '/', '*')
+
+    fun getCurrentExpression(): MutableLiveData<String> {
+        if (mCurrentExpression == null) {
+            mCurrentExpression = MutableLiveData()
+        }
+        return mCurrentExpression as MutableLiveData<String>
+    }
+
+    fun getResult(): MutableLiveData<String> {
+        if (mResult == null) {
+            mResult = MutableLiveData()
+        }
+        return mResult as MutableLiveData<String>
+    }
+
+    fun onOperatorAdd(addedValue: String) {
+        if (currentExpressionIsInvalid() && addedValue == STRING_COMMA || addedValue == PERCENTAGE) {
+            /**TODO: HOW TO DO THIS?
+            view.showInvalidExpressionMessage()**/
+        } else {
+            var isCommaAddedToExpression = false
+
+            if ((isValueAnOperator(addedValue) || addedValue == PERCENTAGE) && mCurrentExpression!!.value!!.isNotEmpty()) {
+                val lastCharacterOfExpression = mCurrentExpression!!.value!!.get(mCurrentExpression!!.value!!.length - 1)
+
+                if (isValueAnOperator(lastCharacterOfExpression.toString())) {
+                    clearLastCharOfExpression()
+                }
+            } else if (addedValue == STRING_COMMA) {
+                val expressionArray = mCurrentExpression!!.value!!.toCharArray()
+                for (c in expressionArray) {
+                    if (c == STRING_COMMA.toCharArray()[0]) {
+                        isCommaAddedToExpression = true
+                    }
+                    if (validOperators.contains(c)) {
+                        isCommaAddedToExpression = false
+                    }
+                }
+
+                // If last character of expression is either a number or an operator, do not add the comma to the expression.
+                val lastCharacterOfExpression = mCurrentExpression!!.value!!.get(mCurrentExpression!!.value!!.length - 1)
+                if (validOperators.contains(lastCharacterOfExpression)) {
+                    isCommaAddedToExpression = true
+                }
+            }
+
+            if (!isCommaAddedToExpression) {
+                if (mCurrentExpression!!.value == null) {
+                    mCurrentExpression!!.value = addedValue
+                } else {
+                    mCurrentExpression!!.value += addedValue
+                }
+            }
+        }
+    }
+
+    fun onClearExpression() {
+        mCurrentExpression!!.value = ""
+        mResult!!.value = ""
+    }
+
+    fun onCalculateResult() {
+        if (mCurrentExpression!!.value == null || mCurrentExpression!!.value!!.contains(INFINITY)) {
+            /**TODO: HOW TO DO THIS?
+            view.showInvalidExpressionMessage()**/
+        } else {
+            clearLastValueIfItIsAnOperator()
+
+            mCurrentExpression!!.value = mCurrentExpression!!.value!!.replace(PERCENTAGE.toRegex(), "/100")
+
+            val expression = Expression(mCurrentExpression!!.value!!)
+
+            val bigDecimalResult = expression.eval()
+
+            val doubleResult = bigDecimalResult.toDouble()
+
+            val stringResult: String
+
+            if (isValueInteger(doubleResult) && !isScientificNotation(java.lang.Double.toString(doubleResult))) {
+                val roundedValue = Math.round(doubleResult).toInt()
+                stringResult = roundedValue.toString()
+            } else {
+                stringResult = java.lang.Double.toString(doubleResult)
+            }
+
+            mCurrentExpression!!.value = stringResult
+            mResult!!.value = stringResult
+        }
+    }
+
+    fun onExpressionSignChange() {
+        if (currentExpressionIsInvalid()) {
+            //SHOW INVALID EXPRESSION MESSAGE HERE
+        } else {
+            mCurrentExpression!!.value = if (isNumberPositive)
+                "-${mCurrentExpression!!.value}"
+            else
+                mCurrentExpression!!.value!!.substring(1, mCurrentExpression!!.value!!.length)
+
+            isNumberPositive = !isNumberPositive
+        }
+    }
+
+    private fun currentExpressionIsInvalid() =
+            mCurrentExpression!!.value == null || mCurrentExpression!!.value!!.isEmpty()
+
+    private fun isValueAnOperator(value: String): Boolean {
+        return validOperators.contains(value.toCharArray()[0])
+    }
+
+    private fun clearLastCharOfExpression() {
+        mCurrentExpression!!.value = mCurrentExpression!!.value!!.substring(0, mCurrentExpression!!.value!!.length - 1)
+    }
+
+    private fun clearLastValueIfItIsAnOperator() {
+        if (isValueAnOperator(getLastCharOfExpression().toString())) {
+            clearLastCharOfExpression()
+        }
+    }
+
+    private fun getLastCharOfExpression(): Char {
+        val currentExpressionLastValuePosition = mCurrentExpression!!.value!!.length - 1
+        return mCurrentExpression!!.value!![currentExpressionLastValuePosition]
+    }
+
+    private fun isValueInteger(number: Double): Boolean {
+        val roundedValue = Math.round(number).toInt()
+        return number % roundedValue == 0.0
+    }
+
+    private fun isScientificNotation(numberString: String): Boolean {
+        try {
+            BigDecimal(numberString)
+        } catch (e: NumberFormatException) {
+            return false
+        }
+
+        return numberString.toUpperCase().contains(SCIENTIFIC_NOTATION_CHAR)
+    }
+}
--- calculator/src/main/java/zurche/simplecalculator/calculator/MainActivity.kt
+++ calculator/src/main/java/zurche/simplecalculator/calculator/MainActivity.kt
@@ -1,50 +1,63 @@
 package zurche.simplecalculator.calculator
 
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), CalculatorContract.View {
+class MainActivity : AppCompatActivity() {
 
-    private val presenter = Presenter(this)
+    private var mCalculatorViewModel: CalculatorViewModel? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        zero.setOnClickListener { presenter.onOperatorAdd(getString(R.string.zero)) }
-        one.setOnClickListener { presenter.onOperatorAdd(getString(R.string.one)) }
-        two.setOnClickListener { presenter.onOperatorAdd(getString(R.string.two)) }
-        three.setOnClickListener { presenter.onOperatorAdd(getString(R.string.three)) }
-        four.setOnClickListener { presenter.onOperatorAdd(getString(R.string.four)) }
-        five.setOnClickListener { presenter.onOperatorAdd(getString(R.string.five)) }
-        six.setOnClickListener { presenter.onOperatorAdd(getString(R.string.six)) }
-        seven.setOnClickListener { presenter.onOperatorAdd(getString(R.string.seven)) }
-        eight.setOnClickListener { presenter.onOperatorAdd(getString(R.string.eight)) }
-        nine.setOnClickListener { presenter.onOperatorAdd(getString(R.string.nine)) }
+        // Get the ViewModel.
+        mCalculatorViewModel = ViewModelProviders.of(this).get(CalculatorViewModel::class.java)
 
-        ac.setOnClickListener { presenter.onClearExpression() }
-        plus_minus_switch.setOnClickListener { presenter.onExpressionSignChange() }
-        percentage.setOnClickListener { presenter.onOperatorAdd(getString(R.string.percentage)) }
-        comma.setOnClickListener { presenter.onOperatorAdd(getString(R.string.comma_expression)) }
-        equals.setOnClickListener { presenter.onCalculateResult() }
+        // Create the observer which updates the UI.
+        val expressionObserver = Observer<String> { newOperation ->
+            // Update the UI, in this case, a TextView.
+            operation.text = newOperation
+        }
 
-        divide.setOnClickListener { presenter.onOperatorAdd(getString(R.string.divide_operator)) }
-        multiply.setOnClickListener { presenter.onOperatorAdd(getString(R.string.multiply_expression)) }
-        plus.setOnClickListener { presenter.onOperatorAdd(getString(R.string.plus)) }
-        minus.setOnClickListener { presenter.onOperatorAdd(getString(R.string.minus)) }
+        // Create the observer which updates the UI.
+        val resultObserver = Observer<String> { result ->
+            // Update the UI, in this case, a TextView.
+            operation_result.text = result
+        }
+
+        // Observe the LiveData, passing in this activity as the LifecycleOwner and the observer.
+        mCalculatorViewModel!!.getCurrentExpression().observe(this, expressionObserver)
+        mCalculatorViewModel!!.getResult().observe(this, resultObserver)
+
+        zero.setOnClickListener { mCalculatorViewModel!!.onOperatorAdd(getString(R.string.zero)) }
+        one.setOnClickListener { mCalculatorViewModel!!.onOperatorAdd(getString(R.string.one)) }
+        two.setOnClickListener { mCalculatorViewModel!!.onOperatorAdd(getString(R.string.two)) }
+        three.setOnClickListener { mCalculatorViewModel!!.onOperatorAdd(getString(R.string.three)) }
+        four.setOnClickListener { mCalculatorViewModel!!.onOperatorAdd(getString(R.string.four)) }
+        five.setOnClickListener { mCalculatorViewModel!!.onOperatorAdd(getString(R.string.five)) }
+        six.setOnClickListener { mCalculatorViewModel!!.onOperatorAdd(getString(R.string.six)) }
+        seven.setOnClickListener { mCalculatorViewModel!!.onOperatorAdd(getString(R.string.seven)) }
+        eight.setOnClickListener { mCalculatorViewModel!!.onOperatorAdd(getString(R.string.eight)) }
+        nine.setOnClickListener { mCalculatorViewModel!!.onOperatorAdd(getString(R.string.nine)) }
+
+        ac.setOnClickListener { mCalculatorViewModel!!.onClearExpression() }
+        plus_minus_switch.setOnClickListener { mCalculatorViewModel!!.onExpressionSignChange() }
+        percentage.setOnClickListener { mCalculatorViewModel!!.onOperatorAdd(getString(R.string.percentage)) }
+        comma.setOnClickListener { mCalculatorViewModel!!.onOperatorAdd(getString(R.string.comma_expression)) }
+        equals.setOnClickListener { mCalculatorViewModel!!.onCalculateResult() }
+
+        divide.setOnClickListener { mCalculatorViewModel!!.onOperatorAdd(getString(R.string.divide_operator)) }
+        multiply.setOnClickListener { mCalculatorViewModel!!.onOperatorAdd(getString(R.string.multiply_expression)) }
+        plus.setOnClickListener { mCalculatorViewModel!!.onOperatorAdd(getString(R.string.plus)) }
+        minus.setOnClickListener { mCalculatorViewModel!!.onOperatorAdd(getString(R.string.minus)) }
     }
 
-    override fun showResult(result: String) {
-        operation_result.text = result
+    /**    override fun showInvalidExpressionMessage() {
+    Toast.makeText(this, getString(R.string.invalid_expression_message), Toast.LENGTH_LONG).show()
     }
-
-    override fun updateCurrentExpression(updatedExpression: String) {
-        operation.text = updatedExpression
-    }
-
-    override fun showInvalidExpressionMessage() {
-        Toast.makeText(this, getString(R.string.invalid_expression_message), Toast.LENGTH_LONG).show()
-    }
+     **/
 }
